43a2fc8c29f8cf55726bfc9924b31d57e48bf422
masoodoo <masoodmasoodi2233@gmail.com>
2025-04-02 14:38:18 +0330

    Add files via upload

---
+++ SecurePasswordManager/SecurePasswordManager/AddPasswordPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using SecurePasswordManager.Models;
+using SecurePasswordManager.Services;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace SecurePasswordManager
+{
+    public partial class AddPasswordPage : Page
+    {
+        public AddPasswordPage()
+        {
+            InitializeComponent();
+        }
+        private void SavePassword_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. ذخیره اطلاعات
+            var encrypted = CryptoHelper.Encrypt(txtPassword.Password, LoginWindow.MasterPassword);
+
+            var newEntry = new PasswordEntry
+            {
+                Name = txtServiceName.Text,
+                EncryptedPassword = encrypted.EncryptedData,
+                Salt = encrypted.Salt,
+                IV = encrypted.IV
+            };
+
+            var passwords = PasswordService.LoadPasswords(LoginWindow.MasterPassword);
+            passwords.Add(newEntry);
+            PasswordService.SavePasswords(passwords, LoginWindow.MasterPassword);
+
+            // 2. بازگشت به صفحه اصلی و به‌روزرسانی لیست
+            if (Window.GetWindow(this) is MainWindow mainWindow)
+            {
+                mainWindow.ReturnFromAddPasswordPage();
+            }
+
+            // 3. اگر از Navigation استفاده می‌کنید
+            if (NavigationService?.CanGoBack == true)
+            {
+                NavigationService.GoBack();
+            }
+        }
+
+    }
+}
---
+++ SecurePasswordManager/SecurePasswordManager/LoginWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using SecurePasswordManager.Services;
+
+namespace SecurePasswordManager
+{
+   
+        public partial class LoginWindow : Window
+        {
+            // رمز اصلی برنامه (موقت - در نسخه نهایی باید از روش امن‌تری استفاده کنید)
+            private const string CorrectPassword = "masood1234";
+
+            public static string MasterPassword { get; private set; }
+
+            public LoginWindow()
+            {
+                InitializeComponent();
+            }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (PasswordConfigService.VerifyPassword(txtMasterPassword.Password))
+            {
+                MasterPassword = txtMasterPassword.Password;
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("رمز عبور نامعتبر است", "خطا", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+    
+}
